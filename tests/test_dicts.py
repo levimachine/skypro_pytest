bb0474f40746a66b5d14ec3bc55c7162add1d61a
Евгений Евстратьев <levipupsik321@gmail.com>
2023-06-07 23:53:13 +0300

    запорол первый пулреквест

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -2,7 +2,7 @@ import pytest
 
 from utils.dicts import get_val
 
-
+# Тесты
 def test_get_val():
     assert get_val({'1': 'hello'}, '1') == 'hello'
     assert get_val({'1': 'hello'}, '1', 'git') == 'hello'
